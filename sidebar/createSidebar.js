52d98ceeacd9737f6a431ab9870330609ad50032
arsine0 <raphael.caballegan@gmail.com>
2021-03-10 10:46:44 +0800

    added sidebar contents

---
+++ sidebar/createSidebar.js
@@ -0,0 +1,7 @@
+var sidebar_el = 
+`<div id="spanCenter">
+    <div id="sidebar">
+        <iframe src="https://docs.erpnext.com/docs/user/manual/en"></iframe>
+    </div>
+</div>`;
+$('body').append(sidebar_el);
--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,21 +1,8 @@
-// function openManual() {
-//     alert('test');
-// }
-
-
 chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
-    if (req.action == "openManual") {
-        var sidebar_el = `<div id="sidebar"></div>`;
-        $('body').append(sidebar_el);
+    switch(req.action) {
+        case "openManual":
+            $('#spanCenter').fadeToggle("slow");
+            break;
     }
     sendResponse({response: "sidebar opened!"});
 });
-
-// chrome.runtime.onConnect.addListener((port) => {
-//     port.onMessage.addListener((msg) => {
-//         if (msg.action == "openManual") {
-//             console.log('action received!');
-//         }
-//         return true;
-//     })
-// })
