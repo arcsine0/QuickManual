593d09074d6f0384d5f98a9658e7cf842ecd9b00
arsine0 <raphael.caballegan@gmail.com>
2021-03-22 14:00:55 +0800

    v0.0.2

--- sidebar/createSidebar.js
+++ sidebar/createSidebar.js
@@ -1,28 +1,33 @@
-var context = 'introduction';
-var contextA = 'def';
-var prevContext;
+var context = 'desk';
 function loadManualContent() {
-    var url_context = "https://quick-manual.herokuapp.com/" + context + '/' + contextA;
-    console.log(url_context);
-    $.ajax({
-        method: "GET",
-        url: url_context,     
-        dataType: "html",
-        success: (data) => {
-            var manualContent = $('#sidebar-content');
-            manualContent.html(data);
-            
-        },
-        error: (err) => {
-            console.log(err);
+    var dataObj = {};
+    var manualContent = $('#sidebar-content');
+    var url_context = "https://quick-manual.herokuapp.com/" + context;
+    chrome.storage.local.get(null, result => {
+        if (result[context] != undefined) { 
+            manualContent.html(result[context]);
         }
-    });
+        else {
+            $.ajax({
+                method: "GET",
+                url: url_context,     
+                dataType: "html",
+                success: (data) => {
+                    manualContent.html(data); 
+                    dataObj[context] = data.toString();
+                    chrome.storage.local.set(dataObj, () => { console.log(`data stored: ${context}`); });
+                },
+                error: (err) => {
+                    console.log(err);
+                }
+            });
+        }
+    })
 }
 
 function reloadManualContent() {
     $('#sidebar-content').empty();
     loadManualContent();
-    console.log(prevContext);
 }
 $(document).ready(function() {
     
@@ -41,33 +46,15 @@ $(document).ready(function() {
 
 chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
     if (req.status === 'complete') {
-        // get query from url algo
         var urlArr = req.url.split('/');
         var queryArr = new Array();
         var storeUrl = false;
         for(urlStr of urlArr) {
-            if (urlStr.includes('desk#')) { storeUrl = true; } 
+            if (urlStr.includes('desk')) { storeUrl = true; } 
             if (storeUrl === true) { queryArr.push(urlStr); }
         }
-        console.log(queryArr);
-        if (queryArr[1] != undefined) {
-            if (queryArr[0] != 'desk#List') {
-                context = queryArr[1].toLowerCase();
-                contextA = 'def';
-            }
-            else {
-                context = prevContext;
-                if (queryArr[1].includes('%20')) {
-                    contextA = queryArr[1].replace('%20', '-').toLowerCase();
-                }
-                else {
-                    contextA = queryArr[1].toLowerCase();
-                }
-            }
-        }
+        if (queryArr[1] != undefined) { context = queryArr[1].toLowerCase(); }
         reloadManualContent();
-        prevContext = context;
     }
-    
     sendResponse({ });
 })
